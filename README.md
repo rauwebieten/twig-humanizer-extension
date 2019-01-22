75c77a88e6b1ffd0ec19c221df77e95f5f937517
rauwebieten <rauwe.bieten@gmail.com>
2019-01-22 18:29:25 +0100

    refactor filter names for compatibility

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Twig Humanizer Extension
 
+[![Maintainability](https://api.codeclimate.com/v1/badges/5143e3f5b20f57a3084d/maintainability)](https://codeclimate.com/github/rauwebieten/twig-humanizer-extension/maintainability)
+
 A [Twig extension](https://github.com/twigphp/Twig)
 for the [PHP Humanizer library](https://github.com/coduo/php-humanizer)
 
@@ -13,20 +15,20 @@ composer require rauwebieten/twig-humanizer-extension
 
 ## Available filters
 
-humanize,
-truncate,
-truncate_html,
-ordinalize,
-ordinal,
-to_roman,
-from_roman,
-binary_suffix,
-precise_binary_suffix,
-metric_suffix,
-metric_suffix,
-oxford,
-difference,
-precise_difference
+- humanizer_humanize
+- humanizer_truncate
+- humanizer_truncate_html
+- humanizer_ordinalize
+- humanizer_ordinal
+- humanizer_to_roman
+- humanizer_from_roman
+- humanizer_binary_suffix
+- humanizer_precise_binary_suffix
+- humanizer_metric_suffix
+- humanizer_metric_suffix
+- humanizer_oxford
+- humanizer_difference
+- humanizer_precise_difference
 
 See the [PHP Humanizer documentation](https://github.com/coduo/php-humanizer) for details.
 
--- src/RauweBieten/TwigHumanizerExtension.php
+++ src/RauweBieten/TwigHumanizerExtension.php
@@ -12,23 +12,23 @@ class TwigHumanizerExtension extends \Twig_Extension
     public function getFilters()
     {
         return [
-            new \Twig_Filter('humanize', [StringHumanizer::class, 'humanize']),
-            new \Twig_Filter('truncate', [StringHumanizer::class, 'truncate']),
-            new \Twig_Filter('truncate_html', [StringHumanizer::class, 'truncateHtml']),
+            new \Twig_Filter('humanizer_humanize', [StringHumanizer::class, 'humanize']),
+            new \Twig_Filter('humanizer_truncate', [StringHumanizer::class, 'truncate']),
+            new \Twig_Filter('humanizer_truncate_html', [StringHumanizer::class, 'truncateHtml']),
 
-            new \Twig_Filter('ordinalize', [NumberHumanizer::class, 'ordinalize']),
-            new \Twig_Filter('ordinal', [NumberHumanizer::class, 'ordinal']),
-            new \Twig_Filter('to_roman', [NumberHumanizer::class, 'toRoman']),
-            new \Twig_Filter('from_roman', [NumberHumanizer::class, 'fromRoman']),
-            new \Twig_Filter('binary_suffix', [NumberHumanizer::class, 'binarySuffix']),
-            new \Twig_Filter('precise_binary_suffix', [NumberHumanizer::class, 'preciseBinarySuffix']),
-            new \Twig_Filter('metric_suffix', [NumberHumanizer::class, 'metricSuffix']),
-            new \Twig_Filter('metric_suffix', [NumberHumanizer::class, 'metricSuffix']),
+            new \Twig_Filter('humanizer_ordinalize', [NumberHumanizer::class, 'ordinalize']),
+            new \Twig_Filter('humanizer_ordinal', [NumberHumanizer::class, 'ordinal']),
+            new \Twig_Filter('humanizer_to_roman', [NumberHumanizer::class, 'toRoman']),
+            new \Twig_Filter('humanizer_from_roman', [NumberHumanizer::class, 'fromRoman']),
+            new \Twig_Filter('humanizer_binary_suffix', [NumberHumanizer::class, 'binarySuffix']),
+            new \Twig_Filter('humanizer_precise_binary_suffix', [NumberHumanizer::class, 'preciseBinarySuffix']),
+            new \Twig_Filter('humanizer_metric_suffix', [NumberHumanizer::class, 'metricSuffix']),
+            new \Twig_Filter('humanizer_metric_suffix', [NumberHumanizer::class, 'metricSuffix']),
 
-            new \Twig_Filter('oxford', [CollectionHumanizer::class, 'oxford']),
+            new \Twig_Filter('humanizer_oxford', [CollectionHumanizer::class, 'oxford']),
 
-            new \Twig_Filter('difference', [DateTimeHumanizer::class, 'difference']),
-            new \Twig_Filter('precise_difference', [DateTimeHumanizer::class, 'preciseDifference']),
+            new \Twig_Filter('humanizer_difference', [DateTimeHumanizer::class, 'difference']),
+            new \Twig_Filter('humanizer_precise_difference', [DateTimeHumanizer::class, 'preciseDifference']),
         ];
     }
 }
