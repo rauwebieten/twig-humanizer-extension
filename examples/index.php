9acd49febbe5462294912e386406161a9edbd457
peterkinamo <peter@kinamo.be>
2019-01-23 09:03:53 +0100

    cleanup code

--- examples/index.php
+++ examples/index.php
@@ -2,15 +2,14 @@
 
 namespace examples;
 
-use Coduo\PHPHumanizer\NumberHumanizer;
 use RauweBieten\TwigHumanizerExtension;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-require_once  __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader = new FilesystemLoader(__DIR__);
 $twig = new Environment($loader);
 $twig->addExtension(new TwigHumanizerExtension());
 
-echo $twig->render('template.html.twig',[]);
+echo $twig->render('template.html.twig', []);
